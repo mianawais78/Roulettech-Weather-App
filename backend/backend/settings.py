6d49664a134cdd48ef846d5ef9ec96d3fcadfa80
Hafiz Muhammad Awais Hassan <mianawaishassan56@gmail.com>
2024-08-23 18:12:28 -0700

    Deploying to aws

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,13 +114,23 @@ SIMPLE_JWT = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'oaDb',
+        'USER': 'superUser',
+        'PASSWORD': 'Bnhj7890',
+        'HOST': 'oadb.cr2e6qu08gck.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -159,3 +169,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AWS_ACCESS_KEY_ID = 'AKIA4MTWLUKJASDGYK26'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'oabackend '
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
